a7c104d4a80835e13ba9861fe4b25af51d48dfa0
zj.li <zj.li@unistrong.com>
2020-07-28 20:29:54 +0800

    小修改

--- spark-test/src/test/java/com/lzj/AppTest.java
+++ spark-test/src/test/java/com/lzj/AppTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 /**
  * Unit test for simple App.
@@ -20,5 +21,18 @@ public class AppTest
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         Date date = format.parse(str);
 
+    }
+
+    @Test
+    public void mapTest() {
+        HashMap<String, Integer> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
+
+        map1.put("hello", 1);
+        map1.put("world", 1);
+        map2.put("hello", 1);
+
+        Integer hello = map1.merge("hello", 1, Integer::sum);
+        System.out.println(hello);
     }
 }
