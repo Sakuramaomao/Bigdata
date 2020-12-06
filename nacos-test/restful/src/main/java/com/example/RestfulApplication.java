d15d344c0407b11b09bf9dcf9f0eaa41370bb4d7
Sakuramaomao <846562928@qq.com>
2020-12-06 21:35:35 +0800

    增加dubbo-nacos整合

---
+++ nacos-test/restful/src/main/java/com/example/RestfulApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class RestfulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulApplication.class, args);
+    }
+}
