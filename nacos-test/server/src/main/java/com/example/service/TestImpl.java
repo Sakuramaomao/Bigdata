351da56f201685d4834c7499deb577a80b3bdcae
zj.li <zj.li@unistrong.com>
2020-12-08 10:34:38 +0800

    注解更换

--- nacos-test/server/src/main/java/com/example/service/TestImpl.java
+++ nacos-test/server/src/main/java/com/example/service/TestImpl.java
@@ -1,14 +1,14 @@
 package com.example.service;
 
 import com.lzj.api.TestInterface;
-import org.apache.dubbo.config.annotation.Service;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Component;
 
 /**
  * @Author Sakura
  * @Date 2020/12/06 20:23
  */
-@Service
+@DubboService
 @Component
 public class TestImpl implements TestInterface {
     @Override
